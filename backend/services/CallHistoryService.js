30c26026cf1f7e303e4d6af36c92af3605d8b3b8
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2026-05-05 16:21:52 +0500

    Create call history service

---
+++ backend/services/CallHistoryService.js
@@ -0,0 +1,74 @@
+const AbstractService = require('./AbstractService');
+
+class CallHistoryService extends AbstractService {
+   constructor(schemaModel) {
+      super(schemaModel);
+   }
+
+   //result methods
+   async getDocuments(session = null) {
+      return await super.getDocuments(session);
+   }
+
+   async getDocumentById(objectId, session = null) {
+      return await super.getDocumentById(objectId, session);
+   }
+
+   async getDocumentsByCustomFilters(filterConditions, session = null) {
+      return await super.getDocumentsByCustomFilters(filterConditions, session);
+   }
+
+   async getDocumentByCustomFilters(filterConditions, session = null) {
+      return await super.getDocumentByCustomFilters(filterConditions, session);
+   }
+
+   async updateDocument(filterConditions, updateFields, session = null) {
+      return await super.updateDocument(
+         filterConditions,
+         updateFields,
+         session,
+      );
+   }
+
+   async saveDocument(data, session = null) {
+      return await super.saveDocument(data, session);
+   }
+
+   async saveDocuments(data, session = null) {
+      return await super.saveDocuments(data, session);
+   }
+
+   async deleteDocument(filterConditions, session = null) {
+      return await super.deleteDocument(filterConditions, session);
+   }
+
+   async deleteDocumentById(objectId, session = null) {
+      return await super.deleteDocumentById(objectId, session);
+   }
+
+   async deleteDocuments(filterConditions, session = null) {
+      return await super.deleteDocuments(filterConditions, session);
+   }
+
+   //query methods
+   getDocumentsByCustomFiltersQuery(filterConditions, session = null) {
+      return super.getDocumentsByCustomFiltersQuery(filterConditions, session);
+   }
+
+   getDocumentsQuery(session = null) {
+      return super.getDocumentsQuery(session);
+   }
+
+   findLogsInOrderByCreatedAtQuery(
+      filterConditions,
+      order = 1,
+      session = null,
+   ) {
+      return this.schemaModel
+         .find(filterConditions)
+         .session(session)
+         .sort({ createdAt: order }); //ascending order
+   }
+}
+
+module.exports = CallHistoryService;
