ce6a86eae0a758aedcee30f5c5fef15dd4a1eaae
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2026-05-05 20:50:47 +0500

    Create report service

---
+++ backend/services/ReportService.js
@@ -0,0 +1,55 @@
+const AbstractService = require("./AbstractService");
+
+class ReportService extends AbstractService {
+  constructor(schemaModel) {
+    super(schemaModel);
+  }
+
+  //result methods
+  async getDocuments() {
+    return await super.getDocuments();
+  }
+
+  async getDocumentById(objectId) {
+    return await super.getDocumentById(objectId);
+  }
+
+  async getDocumentsByCustomFilters(filterConditions) {
+    return await super.getDocumentsByCustomFilters(filterConditions);
+  }
+
+  async getDocumentByCustomFilters(filterConditions) {
+    return await super.getDocumentByCustomFilters(filterConditions);
+  }
+
+  async updateDocument(filterConditions, updateFields) {
+    return await super.updateDocument(filterConditions, updateFields);
+  }
+
+  async saveDocument(data) {
+    return await super.saveDocument(data);
+  }
+
+  async saveDocuments(data) {
+    return await super.saveDocuments(data);
+  }
+
+  async deleteDocument(filterConditions) {
+    return await super.deleteDocument(filterConditions);
+  }
+
+  async deleteDocumentById(objectId) {
+    return await super.deleteDocumentById(objectId);
+  }
+
+  async deleteDocuments(filterConditions) {
+    return await super.deleteDocuments(filterConditions);
+  }
+
+  //query methods
+  getDocumentsByCustomFiltersQuery(filterConditions) {
+    return super.getDocumentsByCustomFiltersQuery(filterConditions);
+  }
+}
+
+module.exports = ReportService;
